9bf678a85b84a43e94c7924a2d37281aaf4b10f0
Confident Sales <erp-admin@confidentonline.com>
2025-02-26 09:47:23 +0530

    third commit

--- Backend/db.js
+++ Backend/db.js
@@ -3,22 +3,22 @@ const dotenv = require('dotenv')
 
 dotenv.config();
 
-// const pool = new Pool({
-//    user : process.env.USER,
-//    host : process.env.HOST,
-
-//    database : process.env.DATABASE,
-//    password : process.env.PASSWORD,
-//    port : process.env.DB_PORT,
-// })
-
 const pool = new Pool({
-   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-   ssl: { rejectUnauthorized: false }
- })
+   user : process.env.USER,
+   host : process.env.HOST,
+
+   database : process.env.DATABASE,
+   password : process.env.PASSWORD,
+   port : process.env.DB_PORT,
+})
+
+// const pool = new Pool({
+//    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+//    ssl: { rejectUnauthorized: false }
+//  })
  
- pool.connect()
-  .then(() => console.log("Connected to PostgreSQL"))
-  .catch(err => console.error("Connection error", err));
+//  pool.connect()
+//   .then(() => console.log("Connected to PostgreSQL"))
+//   .catch(err => console.error("Connection error", err));
 
 module.exports = pool;
